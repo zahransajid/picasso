73f859585f7e5e625dc9eda05b2f88000d6f6a34
zahransajid <zahransajid3@gmail.com>
2022-09-05 14:03:42 +0530

    Modified test_app.py to accept user input.

--- stylize/utils.py
+++ stylize/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import PIL.Image
 import tensorflow_hub as hub
 from matplotlib import pyplot as plt
-from io import BytesIO
+from io import BytesIO, StringIO
 
 
 def tensor_to_image(tensor: tf.Tensor) -> PIL.Image:
@@ -19,7 +19,7 @@ def tensor_to_image(tensor: tf.Tensor) -> PIL.Image:
     return PIL.Image.fromarray(tensor)
 
 
-def load_img(path_to_img) -> tf.Tensor:
+def load_img(path_to_img : str) -> tf.Tensor:
     """Takes path to file as input, returns a Tensor
     Performs the following operations:
     - Scales the image so the longest dimension of the image shape is 512
@@ -40,6 +40,25 @@ def load_img(path_to_img) -> tf.Tensor:
     img = img[tf.newaxis, :]
     return img
 
+def load_img_from_bytesio(img : BytesIO) -> tf.Tensor:
+    """Takes a BytesIO object as input, returns a Tensor
+    Performs the following operations:
+    - Scales the image so the longest dimension of the image shape is 512
+    - Expands dimensions in front"""
+    max_dim = 512
+    img = tf.image.decode_image(img.read(), channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # Takes the shape of the image, which is a tensor and converts it to an array of 32-bit floats
+    # Also removes the last element (The number of channels)
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
 
 def img_to_bytesio(img: PIL.Image, format="JPEG") -> BytesIO:
     """Converts a PIL Image object to and returns a BytesIO buffer
--- test_app.py
+++ test_app.py
@@ -1,21 +1,19 @@
 import stylize
 import fastapi
+from fastapi import File, UploadFile
 from starlette.responses import StreamingResponse
 from PIL import Image
+from io import StringIO
+import uuid
 
 app = fastapi.FastAPI()
 model = stylize.Model()
-image_path = None
+style_img = stylize.utils.load_img(r"stylize\style_images\Picasso1.jpg")
 
-
-@app.get("/image")
-def image_endpoint():
-    style_img = stylize.utils.load_img(
-        r"stylize\style_images\Picasso1.jpg"
-    )
-    content_img = stylize.utils.load_img(
-        image_path
-    )
+@app.post("/file/")
+def create_upload_file(file: UploadFile = File(...)):
+    content = file.file
+    content_img = stylize.utils.load_img_from_bytesio(content)
     img = model.stylize(content_img, style_img)
     buf = stylize.utils.img_to_bytesio(img, format="PNG")
     return StreamingResponse(buf, media_type="image/png")
