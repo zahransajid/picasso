7d9a402a6bc5101d281bd0c8c576b4b8beab1f9d
Christo <christotantony2003@gmail.com>
2022-09-15 23:16:38 +0530

    added cards to drag and drop images

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {teal, yellow} from '@mui/material/colors';
 import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles'
 import { SnackbarProvider } from 'notistack';
-import { Typography, Button, Grid, Box } from "@mui/material"
+import { Typography, Button, Grid, Box, Card } from "@mui/material"
 import { NavBar } from "./components/NavBar";
 
 let theme = createTheme({
@@ -29,41 +29,95 @@ const App = () => {
             <ThemeProvider theme={theme}>
                 <SnackbarProvider autoHideDuration={2000}>
                     <NavBar />                    
-                        <Grid container spacing={2} sx={{
+                        <Grid container spacing={10} sx={{
                             height:"100%",
+                            width:"100%",
                             display:"flex",
-                            marginTop:"5px"
+                            marginTop:"5px",
+                            marginLeft:"5px",
+
                         }}>
 
-                            <Grid
-                                xs={5}
-                                display='flex'  
-                                justifyContent='center' 
-                                alignContent='center' 
-                                height='100%'>
-                                    <Box 
-                                        component='img'
-                                        sx={{
-                                            width: '70%',
-                                            height: 'auto',
-                                        }}
-                                        src="art_lover.svg">
-                                            
+                            <Grid xs={6} sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}>
+                                <Card sx={{
+                                    width: "80%",
+                                    height: "70%",
+                                    backgroundColor: "rgba(0,0,0, 0.4)",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    borderColor: theme.palette.secondary.main,
+                                    borderRadius: '15px',
+                                    borderStyle: 'solid',
+                                    borderWidth: "2px",
+
+                                }}>
+                                    <Typography marginTop="10px" variant="h3" textAlign="center" color={theme.palette.secondary.main} >
+                                        Image to Stylize
+                                    </Typography>
+                                    
+                                    <Box sx={{
+                                        width: "100%",
+                                        display:"flex",
+                                        marginTop: "10px",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        flex: 1,
+                                    }}>
+                                        <Button sx={{
+                                            width: "90%",
+                                            height: "90%",
+
+                                        }}>
+                                            Select your Image or Drag and Drop
+                                        </Button>
                                     </Box>
+
+                                </Card>
                             </Grid>
 
-                            <Grid xs={7}  className='' justifyContent="center" alignContent='center' display='flex'>
-                                <Typography alignSelf="center" variant="h1" fontWeight='200' textAlign='right' marginRight='20px'>
-                                    Reincarnating art of past with AI of today
-                                </Typography>
-                            </Grid>
+                            <Grid xs={6} sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center"
+                            }}>
+                                <Card sx={{
+                                        width: "80%",
+                                        height: "70%",
+                                        backgroundColor: "rgba(0,0,0, 0.4)",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        borderColor: theme.palette.secondary.main,
+                                        borderRadius: '15px',
+                                        borderStyle: 'solid',
+                                        borderWidth: "2px",
 
-                            <Grid xs={6}>
-                                
-                            </Grid>
+                                    }}>
+                                    <Typography marginTop="10px" variant="h3" textAlign="center" color={theme.palette.secondary.main} >
+                                        Image Style to Transfer
+                                    </Typography>
+                                    
+                                    <Box sx={{
+                                        width: "100%",
+                                        display:"flex",
+                                        marginTop: "10px",
+                                        alignItems: "center",
+                                        justifyContent: "center",
+                                        flex: 1,
+                                    }}>
+                                        <Button sx={{
+                                            width: "90%",
+                                            height: "90%",
 
-                            <Grid xs={6}>
-                                
+                                        }}>
+                                            Select your Image or Drag and Drop
+                                        </Button>
+                                    </Box>
+
+                                </Card>
                             </Grid>
 
                         </Grid>
